bfbc5130b7106c1dcad0ebe297ed736669c5d9d5
Carlos E. Feliz <NotAnEmail6_9@protonmail.com>
2025-09-30 08:40:43 -0400

    change the DB

--- db/pool.js
+++ db/pool.js
@@ -5,8 +5,8 @@ const mysql = require("mysql2");
 // Crea el pool de conexiones usando la sintaxis de mysql2/promise
 const pool = mysql.createPool({
   host: "localhost", // Ahora apunta a tu servidor MySQL
-  user: "linux",
-  password: "unnuevopw4321**",
+  user: "root",
+  password: "rootpw321**",
   database: "QRAPP",
   port: 3306, // Puerto predeterminado de MySQL
   waitForConnections: true,
